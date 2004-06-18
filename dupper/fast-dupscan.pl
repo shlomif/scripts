84f6e46d0d98d33c6e2348baab79c73530d3d412
Jeremy Mates <jmates@sial.org>
2004-06-18 01:55:51 +0000

    Indexes things by size first, figure out how to merge into dupper.

---
+++ dupper/fast-dupscan.pl
@@ -0,0 +1,83 @@
+#!/usr/local/bin/perl -w
+# Craig Reyenga
+
+use strict;
+#use Cwd;
+#use BSD::stat;
+use Digest::MD5;
+#use File::Basename;
+use File::Find;
+#use File::Spec;
+
+#my @files;
+my %sizes;
+
+my $debug=1;
+
+my $dir=shift(@ARGV);
+my $dig=Digest::MD5->new;
+
+find(
+    sub {
+	if (!-l $File::Find::name && -f _) {
+	    my $size=-s $File::Find::name;
+	    if (defined($sizes{$size})) {
+		push(@{$sizes{$size}}, $File::Find::name);
+	    } else {
+	        $sizes{$size}=[$File::Find::name];
+	    }
+	}
+    },$dir);
+
+foreach my $size (sort { $a <=> $b } keys %sizes) {
+    my @aos=@{$sizes{$size}};
+    if ($size == 0) {
+	print "##The following are empty:\n";
+	foreach my $i (@aos) {
+	    print "#$i\n";
+	}
+	next;
+    }
+    my $hm=@aos;
+    if ($hm >= 2) {
+	my %pot;
+        foreach my $i (@aos) {
+            unless (open FILE, $i) {
+	        print STDERR "##Can't open $i: $!\n";
+	        next;
+	    }
+	    $dig->addfile(*FILE);
+	    my $g = $dig->hexdigest;
+	    close FILE;
+	    
+	    if (defined($pot{$g})) {
+		push(@{$pot{$g}}, $i);
+	    } else {
+		$pot{$g}=[$i];
+	    }
+        }
+	foreach my $p (sort keys %pot) {
+	    my @aop=@{$pot{$p}};
+	    my $hmp=@aop;
+	    if ($hmp >= 2) { 
+	        print "##Duplicate: s=$size md5=$p\n";
+		print "#" . shift(@aop) . "\n";
+	        print join("\n", @aop);
+		print "\n";
+	    }
+	}
+	    
+    }
+    
+}
+
+
+__END__
+ } elsif (defined($sums{$g})&&($g ne 'd41d8cd98f00b204e9800998ecf8427e')) {
+  print "Identical files found:\n $f\n $sums{$g}\n";
+ } else {
+  $sums{$g} = $f;
+ }
+} ## end foreach
+
+
