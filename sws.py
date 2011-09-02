1bb644681363bbdea60b1d87c0b918d585a1fed4
Jeremy Mates <jmates@sial.org>
2011-09-02 04:23:53 +0000

    Decorated!

---
+++ sws.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#Significant whitespace!
+
+sum=0
+foriinrange(5):
+ sum=sum+i
+ printi,sum
+
+print
+whilei>0:
+	i=i-1
+	printi
