c95e76c6c78680fb50dd25f47bc1163c71636350
Jeremy Mates <jmates@cpan.org>
2012-11-02 15:36:43 +0000

    poll() to help avoid busy wait edge cases.

--- c/obdurate.c
+++ c/obdurate.c
@@ -1,26 +1,43 @@
 /*
  * Ignore most common signals. Handy to test things that will require up
- * to a SIGKILL to clear from the process table. (Complicated things
- * like forking child processes and then waiting on those and also
- * interacting with associated signals not supported.)
+ * to a SIGKILL to clear from the process table (like, hypothetically, a
+ * horribly wedged LDAP server). Complicated things like forking child
+ * processes and then waiting on those and also interacting with
+ * associated signals not supported.
+ *
+ * PID emitted to stdout, all other output should be to stderr.
+ *
+ * If stdin is not a TTY, a temporary file will be blocked on. This temp
+ * file will not be cleaned up for obvious reasons.
+ *
+ * XXX figure out way just to test whether stdin "is open".
  */
 
 #include <err.h>
+#include <errno.h>
+#include <poll.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sysexits.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 void whine(int sig);
 
 int main()
 {
-    int c;
+    char buf[BUFSIZ];
+    char tmp_filename[20] = "/tmp/obd.XXXXXXXXXX";
+    int nfds;
+    struct pollfd pfd[1];
     struct sigaction act;
 
-//    act.sa_handler = SIG_IGN;   // ignore the signal
+    /*
+     * Setup signal handling
+     */
+//    act.sa_handler = SIG_IGN;   // just ignore the signal
     act.sa_handler = whine;     // whine about the signal
 
     sigemptyset(&act.sa_mask);
@@ -36,6 +53,7 @@ int main()
         err(EX_OSERR, NULL);
     if (sigaction(SIGUSR2, &act, NULL) != 0)
         err(EX_OSERR, NULL);
+    // might happen if stderr closed for some reason?
     if (sigaction(SIGPIPE, &act, NULL) != 0)
         err(EX_OSERR, NULL);
 
@@ -43,12 +61,30 @@ int main()
     if (sigaction(SIGALRM, &act, NULL) != 0)
         err(EX_OSERR, NULL);
 
+    /*
+     * (Blocking) I/O is Tricky
+     */
+
+    // Avoid busy loop if cannot block on input (e.g. started in background)
+    if (isatty(STDIN_FILENO)) {
+        pfd[0].fd = STDIN_FILENO;
+    } else {
+        if ((pfd[0].fd = mkstemp(tmp_filename)) == -1)
+            err(EX_IOERR, "mkstemp failed to create tmp file");
+    }
+    pfd[0].events = POLLPRI;
+
     printf("info: pid %ld\n", (long int) getpid());
 
     for (;;) {
-        while ((c = getchar()) != EOF) {
-            putchar(c);
-        }
+        nfds = poll(pfd, 1, 60 * 1000);
+        if (nfds == -1 || (pfd[0].revents & (POLLERR | POLLHUP | POLLNVAL)))
+            warnx("poll error");
+        else if (nfds == 0)
+            //   warnx("timeout");
+            ;
+        else if (read(pfd[0].fd, buf, sizeof(buf)) == -1)
+            warnx("could not read");
     }
 
     exit(EXIT_SUCCESS);
@@ -56,5 +92,5 @@ int main()
 
 void whine(int sig)
 {
-    warnx("caught signal %d", sig);
+    warnx("caught signal %d at %ld", sig, (long int) time(NULL));
 }
