0fecbf3936dbcab69cb236e79e79f90cb3d73a0a
Jeremy Mates <jmates@cpan.org>
2013-07-17 22:09:58 +0000

    Even more simply.

--- filesys/stripeofeol.c
+++ filesys/stripeofeol.c
@@ -34,10 +34,8 @@ int main(int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
-    if (argc < 1) {
+    if (argc < 1)
         usage();
-        /* NOTREACHED */
-    }
 
     while (*argv) {
         if ((fd = open(*argv, O_RDWR)) == -1)
@@ -51,57 +49,50 @@ int main(int argc, char *argv[])
     exit(EXIT_SUCCESS);
 }
 
+/*
+ * Gist of logic is to step backwards through the file, ending
+ * when a non-newline character is detected, and then truncating
+ * the file at that point. This will be quick for normal files,
+ * but less so for pathological cases of large files consisting
+ * entirely of newlines. (For which a larger buf and looping over
+ * that would help avoid excess system calls.)
+ */
 void trim_file(const int fd, const char *file)
 {
-    int only_newlines;
-    long int start_size, trunc_size, i;
-    char buf[2];
+    char buf;
+    long start_size, trunc_size, offset;
 
     if ((start_size = lseek(fd, 0, SEEK_END)) < 0)
         err(EX_IOERR, "could not seek '%s'", file);
     if (start_size == 0)
         return;                 /* empty file, whatevs */
+
     trunc_size = start_size;
-    only_newlines = 1;
 
-    for (i = 1; i <= start_size; i++) {
-        /*
-         * Gist of logic is to step backwards through the file, ending
-         * when a non-newline character is detected, and then truncating
-         * the file to that point. This will be quick for normal files,
-         * but less so for pathological cases of large files consisting
-         * entirely of newlines.
-         */
-        if (lseek(fd, i * -1, SEEK_END) < 0)
-            err(EX_IOERR, "could not seek '%s'", file);
-        if (read(fd, &buf, 1) < 0)
-            err(EX_IOERR, "could not read '%s'", file);
+    for (offset = start_size; offset > 0; offset--) {
+        if (pread(fd, &buf, 1, offset - 1) != 1)
+            err(EX_IOERR, "could not pread '%s'", file);
 
-        switch (buf[0]) {
+        switch (buf) {
         case '\n':
         case '\r':
             break;
         default:
-            only_newlines = 0;
-            trunc_size = lseek(fd, 0, SEEK_CUR);
-            i = start_size;
+            /* non-newline, stop at this offset */
+            goto TEN;
         }
     }
+  TEN:
+    trunc_size = offset;
 
     /*
-     * Edge case: a file with only one or more newlines will not cause
-     * trunc_size to be updated, and thus would not be truncated.
-     */
-    if (only_newlines == 1)
-        trunc_size = 0;
-
-    /*
-     * Another edge case is if the file is changed between the "find the
+     * An edge case is if the file is changed between the "find the
      * offset code" and this here truncate, but that gets into locking,
-     * or more properly the file workflow and how to avoid locking.
+     * or more properly the file workflow and how to avoid locking, for
+     * which I am fond of rename(2).
      */
     if (trunc_size < start_size)
-        if (ftruncate(fd, trunc_size) != 0)
+        if (ftruncate(fd, trunc_size) == -1)
             err(EX_IOERR, "could not truncate '%s'", file);
 }
 
