9d0b22fd87f76cff57276ffcd9d0f748b5154010
Jeremy Mates <jmates@uw.edu>
2013-07-26 22:01:41 +0000

    More standard things I do.

--- c-template.c
+++ c-template.c
@@ -8,14 +8,20 @@
 #include <sysexits.h>
 #include <unistd.h>
 
+void emit_help(void);
+
 int main(int argc, char *argv[])
 {
     int ch;
 
-    while ((ch = getopt(argc, argv, "")) != -1) {
+    while ((ch = getopt(argc, argv, "h?")) != -1) {
         switch (ch) {
+        case 'h':
+        case '?':
+            emit_help();
+            /* NOTREACHED */
         default:
-            ;
+            emit_help();
         }
     }
     argc -= optind;
@@ -24,4 +30,10 @@ int main(int argc, char *argv[])
 
 
     exit(EXIT_SUCCESS);
+}
+
+void emit_help(void)
+{
+    fprintf(stderr, "Usage: TODO\n");
+    exit(EX_USAGE);
 }
