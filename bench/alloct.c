cafbc79f315a493ef261dfa964f57b1fbfb44e74
Jeremy Mates <jmates@cpan.org>
2016-04-09 03:57:48 +0000

    Check if malloc returns data with not-zero values in it.

---
+++ bench/alloct.c
@@ -0,0 +1,78 @@
+/*
+ * Test whether malloc zeros memory or not (it does, on certain modern OS).
+ * (This is not really a benchmark script, though one may need to 'usemem'
+ * to fill the memory on a system with random values.)
+ *
+ * Compile w/o optimization, probably.
+ */
+
+#include <err.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+// https://github.com/thrig/goptfoo
+#include <goptfoo.h>
+
+#define MOSTMEMPOSSIBLE ( (ULONG_MAX < SIZE_MAX) ? ULONG_MAX : SIZE_MAX )
+
+unsigned long memsize;
+
+void emit_help(void);
+
+int main(int argc, char *argv[])
+{
+    int ch;
+    int *foop;
+    unsigned long i;
+
+    while ((ch = getopt(argc, argv, "h?m:")) != -1) {
+        switch (ch) {
+        case 'h':
+        case '?':
+        case 'm':
+            memsize = flagtoul(ch, optarg, 1UL, MOSTMEMPOSSIBLE);
+            break;
+        default:
+            emit_help();
+            /* NOTREACHED */
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (memsize == 0)
+        memsize = MOSTMEMPOSSIBLE / sizeof(int);
+
+    while (memsize) {
+        if ((foop = malloc(memsize * sizeof(int))) == NULL) {
+            memsize >>= 1;
+        } else {
+            break;
+        }
+    }
+
+    if (foop) {
+        fprintf(stderr, "info: auto-alloc picks %lu ints\n", memsize);
+    } else {
+        err(EX_OSERR, "could not auto-malloc() %lu ints", memsize);
+    }
+    foop[1234] = 9;
+
+    for (i = 0; i < memsize; i++) {
+        if (*foop++ != 0)
+            abort();
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void emit_help(void)
+{
+    fprintf(stderr, "Usage: alloct [-m max-memory-size]\n");
+    exit(EX_USAGE);
+}
