0a6a30297a52f6dd43b3a9472338a2e1068419c7
Jeremy Mates <jmates@cpan.org>
2016-09-15 05:16:14 +0000

    TCL support. Fix some precedence issues.

--- tty/rdcomm.c
+++ tty/rdcomm.c
@@ -7,7 +7,7 @@
  * version of that utility (as of OpenBSD 6.0).
  *
  * This code mostly to easily(?) read data via serial from an Arduino
- * and learn me some termios(4) stuff.
+ * and learn me some termios(4) (and TCL) stuff.
  *
  * To exit, kill the process or hit control+c or disrupt the serial line.
  */
@@ -19,6 +19,7 @@
 #include <fcntl.h>
 #include <glob.h>
 #include <paths.h>
+#include <pwd.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -31,37 +32,46 @@
 // https://github.com/thrig/goptfoo
 #include <goptfoo.h>
 
+#include <tcl.h>
+
 #define BAUD_MAX 640 * 640
 #define SERIAL_BUF_LEN 80
 
-unsigned long Flag_Baud = B9600;
+speed_t Flag_Baud = B9600;
 bool Flag_Raw;                  // -r  raw, do not defang special ASCII chars
-
-uint8_t serialdata[SERIAL_BUF_LEN];
+unsigned long Flag_MinRead = 1; // -M, for VMIN read
 
 void emit_help(void);
 
 int main(int argc, char *argv[])
 {
-    int ch, device_fd;
+    int ch, device_fd, len;
     char *device = NULL;
+    const char *homedir, *result;
+    char *serialdata, *tclshrc;
     glob_t devglob;
     ssize_t readret;
     struct termios device_tio;
+    Tcl_Interp *Interp = NULL;
+    Tcl_Obj *Script = NULL;
 
-    while ((ch = getopt(argc, argv, "B:h?lru")) != -1) {
+    while ((ch = getopt(argc, argv, "B:e:h?M:r")) != -1) {
         switch (ch) {
         case 'B':
-            Flag_Baud = flagtoul(ch, optarg, 1UL, (unsigned long) BAUD_MAX);
+            Flag_Baud =
+                (speed_t) flagtoul(ch, optarg, 1UL, (unsigned long) BAUD_MAX);
             break;
-        case 'l':
-            setvbuf(stdout, (char *) NULL, _IOLBF, (size_t) 0);
+        case 'e':
+            Script = Tcl_NewStringObj(optarg, -1);
+            Tcl_IncrRefCount(Script);
+            break;
+        case 'M':
+            Flag_MinRead =
+                (speed_t) flagtoul(ch, optarg, 1UL,
+                                   (unsigned long) SERIAL_BUF_LEN);
             break;
         case 'r':
             Flag_Raw = true;
-            break;
-        case 'u':
-            setvbuf(stdout, (char *) NULL, _IONBF, (size_t) 0);
             break;
         case 'h':
         case '?':
@@ -112,7 +122,31 @@ int main(int argc, char *argv[])
         /* NOTREACHED */
     }
 
-    /* Need non-block here so open does not stall forever. Older unix
+    if ((serialdata = malloc(sizeof(char) * (SERIAL_BUF_LEN + 1))) == NULL) {
+        err(EX_OSERR, "could not malloc() serial data buffer");
+    }
+
+    if (Script) {
+        Interp = Tcl_CreateInterp();
+
+        if ((homedir = getenv("HOME")) == NULL) {
+            homedir = getpwuid(getuid())->pw_dir;
+            if (!homedir) {
+                errx(EX_OSERR, "could not determine HOME directory");
+            }
+        }
+        if (asprintf(&tclshrc, "%s/%s", homedir, ".tclshrc") == -1) {
+            err(EX_OSERR, "could not asprintf() tclshrc path");
+        }
+        /* TODO how tell difference between "no such file" and the case
+         * where the user has most verily pooched their code? */
+        Tcl_EvalFile(Interp, tclshrc);
+
+        Tcl_LinkVar(Interp, "_", (void *) &serialdata,
+                    TCL_LINK_STRING | TCL_LINK_READ_ONLY);
+    }
+
+    /* Need non-block here so open does not stall forever. Olden unix
      * used O_NDELAY, but that "should not be used for new applications"
      * (APUE 2nd edition, p.442) */
     // TODO O_NOCTTY needed? cu(1) does not use it
@@ -126,7 +160,7 @@ int main(int argc, char *argv[])
         err(EX_IOERR, "tcgetattr() failed on '%s'", device);
     // see termios(4) though even those docs may not be clear on the
     // whys or whatfors of these various flags...
-    device_tio.c_cc[VMIN] = 1;
+    device_tio.c_cc[VMIN] = Flag_MinRead;
     device_tio.c_cc[VTIME] = 0;
     device_tio.c_cflag &= ~(CSIZE | PARENB);
     device_tio.c_cflag |= CREAD | CS8 | CLOCAL;
@@ -138,24 +172,25 @@ int main(int argc, char *argv[])
     // cu(1) to rdcomm needs ICRNL not set (no output), while arduino to
     // rdcomm does need it set (otherwise doubled newlines). Sigh.
     device_tio.c_iflag &= ~(ISTRIP | ICRNL | IGNBRK);
-    device_tio.c_iflag |= BRKINT;
+    device_tio.c_iflag |= (BRKINT | IGNCR);
     device_tio.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
     device_tio.c_oflag &= ~OPOST;
-    cfsetspeed(&device_tio, (speed_t) Flag_Baud);
+    cfsetspeed(&device_tio, Flag_Baud);
     if (tcsetattr(device_fd, TCSAFLUSH, &device_tio) == -1)
         err(EX_IOERR, "tcsetattr() failed on '%s'", device);
 
     // confirm settings were actually made (APUE does this...)
     if (tcgetattr(device_fd, &device_tio) == -1)
         err(EX_IOERR, "tcgetattr() failed on '%s'", device);
-    if (device_tio.c_cc[VTIME] != 0 || device_tio.c_cc[VMIN] != 1 ||
-        (device_tio.c_cflag & (CSIZE | PARENB | CREAD | CS8 | CLOCAL) !=
+    if (device_tio.c_cc[VTIME] != 0 || device_tio.c_cc[VMIN] != Flag_MinRead ||
+        ((device_tio.c_cflag & (CSIZE | PARENB | CREAD | CS8 | CLOCAL)) !=
          (CREAD | CS8 | CLOCAL))
-        || (device_tio.c_iflag & (ISTRIP | ICRNL | IGNBRK | BRKINT) != BRKINT)
+        || ((device_tio.c_iflag & (ISTRIP | ICRNL | IGNBRK | BRKINT | IGNCR)) !=
+            (BRKINT | IGNCR))
         || (device_tio.c_lflag & (ICANON | ISIG | IEXTEN | ECHO))
         || device_tio.c_oflag & OPOST) {
         errx(EX_OSERR, "did not tcsetattr() properly");
-     }
+    }
 
     fcntl(device_fd, F_SETFL, 0);       // back to blocking mode
 
@@ -163,8 +198,11 @@ int main(int argc, char *argv[])
         if ((readret =
              read(device_fd, serialdata, (size_t) SERIAL_BUF_LEN)) == -1)
             err(EX_IOERR, "read failed on '%s' (%d)", device, errno);
-        if (readret == 0)
+        if (readret == 0) {
             errx(EX_IOERR, "EOF on read of '%s'", device);
+        } else if (readret < 0) {
+            errx(1, "unexpected negative return from read: %ld", readret);
+        }
 
         if (!Flag_Raw) {
             for (ssize_t i = 0; i < readret; i++) {
@@ -178,7 +216,14 @@ int main(int argc, char *argv[])
             }
         }
 
-        fwrite(serialdata, (size_t) readret, 1, stdout);
+        if (Script) {
+            serialdata[readret] = '\0';
+            if (Tcl_EvalObjEx(Interp, Script, TCL_EVAL_GLOBAL) != TCL_OK) {
+                errx(1, "TCL error: %s", Tcl_GetStringResult(Interp));
+            }
+        } else {
+            write(STDOUT_FILENO, serialdata, (size_t) readret);
+        }
     }
 
     exit(1);                    /* NOTREACHED */
@@ -187,6 +232,6 @@ int main(int argc, char *argv[])
 void emit_help(void)
 {
     fprintf(stderr,
-            "Usage: rdcomm [-B baud] [-l | -u] [-r] [/dev/something]\n");
+            "Usage: rdcomm [-B baud] [-e expr] [-M minread] [-r] [/dev/foo]\n");
     exit(EX_USAGE);
 }
