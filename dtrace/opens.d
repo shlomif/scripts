9ab1e0ff428bd4c79f21b6103ff8dac76aeb9de5
Jeremy Mates <jmates@cpan.org>
2016-09-15 22:48:13 +0000

    Autumn cleaning.

--- dtrace/opens.d
+++
@@ -1,6 +0,0 @@
-#!/usr/sbin/dtrace -q -s
-
-syscall::open:entry {
-  self->file = copyinstr(arg0);
-  printf("%Y %s[%d] %s\n", walltimestamp, execname, pid, self->file);
-}
--- segfault.c
+++
@@ -1,6 +0,0 @@
-/* https://en.wikipedia.org/wiki/Segmentation_fault */
-int main()
-{
-    char *s = "hello world";
-    *s = 'H';
-}
