44c8ce5299a5cf773065c6497da6a96daf7802d3
Jeremy Mates <jmates@cpan.org>
2016-10-06 04:01:24 +0000

    Work
     * feed - mark II with specific support for specific REPL only
     * snooze - like sleep(1) only shows time remaning on various signals
     * timeout - move to date-time dir, -F for full wait on timeout

--- date-time/timeout.c
+++ date-time/timeout.c
@@ -1,17 +1,7 @@
 /*
  * Times out the specified command after the specified amount of time.
- * Originally (written in Perl and) motivated by a Sisyphean Java Mess
- * that would not exit itself, but also handy for closing web browsers
- * after some amount of time, or similar needs.
  *
- * Usage:
- *   timeout [-q] -- duration command [command args ...]
- *
- * For example to kill off a browser after some time:
- *   exec timeout -- 11h systrace -a -e xombrero
- *
- * Huh, the Linux tools apparently have a `timeout` thingy. Go figure.
- *
+ *   timeout -- 9h firefox
  */
 
 #include <sys/time.h>
@@ -31,29 +21,33 @@
 
 void emit_help(void);
 void handle_alarm(int sig);
-int parse_duration(char *durp);
-int spec2secs(int d, char c);
+long parse_duration(char *durp);
+long spec2secs(const int d, const char c);
 
-bool Kill_Bill;
+volatile bool Timed_Out;
 
 /* global mostly for the free zeroing of all the various struct fields */
 struct itimerval iTimer;
 
+bool Flag_Fullwait;             /* -F */
 bool Flag_Quiet;                /* -q */
 
 int main(int argc, char *argv[])
 {
     int ch, exit_status, status;
-    pid_t bill;
+    pid_t pid;
+    long duration;
 
     exit_status = EXIT_SUCCESS;
 
-    while ((ch = getopt(argc, argv, "h?q")) != -1) {
+    while ((ch = getopt(argc, argv, "Fh?q")) != -1) {
         switch (ch) {
+        case 'F':
+            Flag_Fullwait = true;
+            break;
         case 'q':
             Flag_Quiet = true;
             break;
-
         case 'h':
         case '?':
         default:
@@ -67,48 +61,50 @@ int main(int argc, char *argv[])
     if (argc < 2)
         emit_help();
 
-    iTimer.it_value.tv_sec = parse_duration(*argv++);
+    iTimer.it_value.tv_sec = duration = parse_duration(*argv++);
 
-    bill = vfork();
-    if (bill == 0) {            /* child */
+    pid = vfork();
+    if (pid == 0) {             /* child */
         if (execvp(*argv, argv) == -1)
             err(EX_OSERR, "could not exec %s", *argv);
         /* NOTREACHED */
 
-    } else if (bill > 0) {      /* parent */
+    } else if (pid > 0) {       /* parent */
         /* do not restart the wait() after SIGALRM, skip to the end... */
         if (siginterrupt(SIGALRM, 1) == -1)
             err(EX_SOFTWARE, "could not siginterrupt()");
 
         if (signal(SIGALRM, handle_alarm) == SIG_ERR)
-            err(EX_SOFTWARE, "could not setup signal()");
+            err(EX_SOFTWARE, "could not handle ALRM signal()");
 
         if (setitimer(ITIMER_REAL, &iTimer, NULL) == -1)
             err(EX_SOFTWARE, "could not setitimer()");
 
         wait(&status);
 
-        /* the end */
-        if (Kill_Bill) {
+        if (Timed_Out) {
             if (!Flag_Quiet)
                 warnx("duration %ld exceeded: killing pid %d",
-                      (long)iTimer.it_value.tv_sec, bill);
-            /*
-             * Assume child is well behaved, and does not deadlock or
+                      (long) iTimer.it_value.tv_sec, pid);
+            /* Assume child is well behaved, and does not deadlock or
              * otherwise require multiple signals (race condition risk) to
-             * take down.
-             */
-            if (kill(bill, SIGTERM) == -1)
-                err(EX_OSERR, "could not kill child pid %d", bill);
+             * take down. */
+            if (kill(pid, SIGTERM) == -1)
+                err(EX_OSERR, "could not kill child pid %d", pid);
 
             exit_status = EXIT_TIMEOUT;
         } else {
-            /*
-             * Pass on the child exit status. These can be illustrated
+            /* Process did not time out, so in theory there's still a
+             * pending SIGALRM set to arrive from the iTimer. Wait
+             * for that. */
+            if (Flag_Fullwait)
+                sleep(duration);
+
+            /* Pass on the child exit status. These can be illustrated
              * via something like:
              *
-             *   timeout 99 perl -e 'exit 42'      ; echo $?
-             *   timeout 99 perl -e 'kill 15, $$'  ; echo $?
+             *   timeout -- 99 perl -e 'exit 42'      ; echo $?
+             *   timeout -- 99 perl -e 'kill 15, $$'  ; echo $?
              */
             if (WIFEXITED(status))
                 exit_status = WEXITSTATUS(status);
@@ -124,53 +120,54 @@ int main(int argc, char *argv[])
 
 void emit_help(void)
 {
-    fprintf(stderr, "Usage: timeout [-q] -- duration command [cmd args ...]\n");
+    fprintf(stderr, "Usage: timeout [-F] [-q] -- dur command [cmd args ..]\n");
     exit(EX_USAGE);
 }
 
 void handle_alarm(int sig)
 {
-    Kill_Bill = true;
+    Timed_Out = true;
 }
 
 /*
  * Parse the duration, could either be just "42" or "1m3s"
  */
-int parse_duration(char *durp)
+long parse_duration(char *durp)
 {
     char duration_spec;
-    int advance, ret, sleep_duration, tmp_duration;
+    int advance, ret;
+    unsigned int duration = 0;
+    long sleep_duration = 0;
 
-    sleep_duration = tmp_duration = 0;
-
-    ret = sscanf(durp, "%9d%n", &tmp_duration, &advance);
+    // limit here of 9 so cannot overflow unsigned int
+    ret = sscanf(durp, "%9u%n", &duration, &advance);
     if (ret != 1)
         errx(EX_DATAERR,
-             "duration must be a positive integer or short form (e.g. 62 or 1m2s)");
+             "duration must be positive integer or short form (e.g. 62 or 1m2s)");
     durp += advance;
 
     ret = sscanf(durp, "%1c%n", &duration_spec, &advance);
     if (ret == 0 || ret == EOF) {
         /* no following character, so just a raw value */
-        sleep_duration = tmp_duration;
+        sleep_duration = duration;
 
     } else if (ret == 1) {
         /* human short form, perhaps */
         durp += advance;
-        sleep_duration += spec2secs(tmp_duration, duration_spec);
+        sleep_duration += spec2secs(duration, duration_spec);
 
         while ((ret =
-                sscanf(durp, "%9d%1c%n", &tmp_duration, &duration_spec,
+                sscanf(durp, "%9u%1c%n", &duration, &duration_spec,
                        &advance)) == 2) {
             durp += advance;
-            sleep_duration += spec2secs(tmp_duration, duration_spec);
+            sleep_duration += spec2secs(duration, duration_spec);
         }
 
     } else {
-        /* should not reach here. curious if do. */
-        err(EX_SOFTWARE, "unknown return '%d' while parsing duration", ret);
+        err(EX_SOFTWARE, "unknown return '%d' while parsing duration??", ret);
     }
 
+    /* An attacker could make the timeout stupidly large, but not negative */
     if (sleep_duration < 1)
         err(EX_DATAERR, "duration must work out to a positive integer");
 
@@ -178,9 +175,9 @@ int parse_duration(char *durp)
 }
 
 /* converts 3, "m" or whatever into an appropriate number of seconds */
-int spec2secs(int d, char c)
+long spec2secs(const int d, const char c)
 {
-    int new_duration = 0;
+    long new_duration = 0;
 
     switch (c) {
     case 's':
