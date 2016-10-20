c54ee5c98c4f577687b1f3c5dc796884d0475057
Jeremy Mates <jmates@cpan.org>
2016-10-20 18:29:14 +0000

    Ignore control+c generated foreground process group signal.

--- misc/copycat.c
+++ misc/copycat.c
@@ -1,28 +1,9 @@
-/*
- * Copies STDIN to both STDOUT and to the clipboard command defined by the
- * CLIPBOARD environment variable. This could also be done with a FIFO, which
- * may be more suitable in certain contexts:
- *
- *   mkfifo asdf
- *
- * And then:
- *
- *   $CLIPBOARD < asdf &
- *   tee asdf
- *
- * Assuming that the FIFO can be properly created, etc. Otherwise mostly
- * motivated by :%!pbcopy in vi annoyingly filtering out the thus copied data,
- * and the code is otherwise mostly lifted from APUE.
- *
- * See the copycat.1 file for additional documentation (or copy that file to a
- * man1 directory and then `man copycat`). X11-enabled vim users could instead
- * just use the ":%y *" or equivalent to interact with the X11 clipboard via
- * the special * register.
- */
+/* Copies standard input to standard output and to a clipboard command. */
 
 #include <sys/wait.h>
 
 #include <err.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,10 +11,12 @@
 #include <unistd.h>
 
 #define BUFSIZE 4096
-/* This is for Mac OS X; see the man page for details on creating a platform
- * specific wrapper as necessary for xsel or the like. Or, adjust the execlp
- * call below to call the appropriate program directly, though bear in mind
- * xsel(1) may do the Wrong Thing if called directly under vi(1). */
+
+/* This is for Mac OS X; see the man page for details on creating a
+ * platform specific wrapper as necessary for xsel or the like. Or,
+ * adjust the execlp call below to call the appropriate program
+ * directly, though bear in mind xsel(1) may do the Wrong Thing if
+ * called directly under vi(1). */
 #define CLIPBOARD "pbcopy"
 
 int main(void)
@@ -41,10 +24,20 @@ int main(void)
     char buf[BUFSIZE], *clippy, *shortname;
     int fd[2];
     pid_t pid;
+    sigset_t blockthese;
     ssize_t howmuch;
 
     if (pipe(fd) == -1)
         err(EX_OSERR, "pipe() failed");
+
+    /* Ignore control+c generated signal to the foreground process
+     * group; this allows one to collect dynamic output such as `ping
+     * ... | copycat` as otherwise the INT results in no clipboard data
+     * being set. NOTE may be too clever for own good. */
+    sigemptyset(&blockthese);
+    sigaddset(&blockthese, SIGINT);
+    if (sigprocmask(SIG_BLOCK, &blockthese, NULL) == -1)
+        err(EX_OSERR, "could not set sigprocmask of %d", blockthese);
 
     if ((pid = vfork()) < 0) {
         err(EX_OSERR, "fork() failed");
