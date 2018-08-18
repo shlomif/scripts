c39991cbe6247fda40cb3a20bec28f3ccbb1b0bb
Jeremy Mates <jmates@cpan.org>
2018-08-17 20:35:16 -0700

    solitary - exec wrapper to dissociate from the parent

---
+++ misc/solitary.c
@@ -0,0 +1,52 @@
+/* solitary - exec wrapper to dissociate from the parent; similar to the
+ * code presented in "Complete Dissociation of Child from Parent" in
+ * perlipc(1). a double fork, if necessary, would need to be added */
+
+#include <err.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    pid_t pid;
+
+    if (chdir("/") == -1)
+        err(EX_OSERR, "chdir failed");
+
+    if ((fd = open("/dev/null", O_RDONLY)) == -1)
+        err(EX_OSERR, "open failed");
+    if (dup2(fd, STDIN_FILENO) == -1)
+        err(EX_OSERR, "dup2 failed");
+    if (close(fd) == -1)
+        err(EX_OSERR, "close failed");
+
+    if ((fd = open("/dev/null", O_WRONLY)) == -1)
+        err(EX_OSERR, "open failed");
+    if (dup2(fd, STDOUT_FILENO) == -1)
+        err(EX_OSERR, "dup2 failed");
+    if (close(fd) == -1)
+        err(EX_OSERR, "close failed");
+
+    if ((pid = fork()) == -1)
+        err(EX_OSERR, "fork failed");
+    if (pid > 0)
+        exit(0);
+
+    /* child */
+    if (setsid() == -1)
+        err(EX_OSERR, "setsid failed");
+
+    if ((fd = dup(STDOUT_FILENO)) == -1)
+        err(EX_OSERR, "dup failed");
+    if (dup2(fd, STDERR_FILENO) == -1)
+        err(EX_OSERR, "dup2 failed");
+    if (close(fd) == -1)
+        err(EX_OSERR, "close failed");
+
+    argv++;
+    execvp(*argv, argv);
+    exit(EX_OSERR);
+}
