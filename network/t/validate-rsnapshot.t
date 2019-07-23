cede525edda1b676fe3ee1bc53f38523636b2e32
Jeremy Mates <jmates@cpan.org>
2019-07-23 07:40:52 -0700

    some network script additions and improvements

---
+++ network/t/validate-rsnapshot.t
@@ -0,0 +1,61 @@
+#!perl
+use lib qw(../lib/perl5);
+use UtilityTestBelt;
+
+delete $ENV{SSH_ORIGINAL_COMMAND};
+my $soc = 'rsync --server --sender -vnlHogDtprRxSe.iLsfxC --numeric-ids . ';
+
+my $test_prog = './validate-rsnapshot';
+
+# most of the code however involves syslog errors (hard to test) and
+# running specific rsync commands (also hard to test)
+my @tests = (
+    # no SSH_ORIGINAL_COMMAND
+    {   args        => '-n',
+        exit_status => 2,
+    },
+    # should not match. hopefully not run.
+    {   args        => '-n',
+        env         => { SSH_ORIGINAL_COMMAND => 'rm -rf /' },
+        exit_status => 4,
+    },
+    # assumes "t" test directory exists and is accessible
+    {   args        => '-n',
+        env         => { SSH_ORIGINAL_COMMAND => $soc . 't' },
+        exit_status => 0,
+        stdout      => qr(^t$),
+    },
+    # NOTE some vendors put files into this directory (??)
+    {   args        => '-n',
+        env         => { SSH_ORIGINAL_COMMAND => $soc . '/var/empty/' . $$ },
+        exit_status => 5,
+    },
+    {   args        => '-h',
+        env         => { SSH_ORIGINAL_COMMAND => $soc . '/' },
+        exit_status => 64,
+        stderr      => qr/supported/,
+    },
+);
+my $testcmd = Test::Cmd->new(prog => $test_prog, workdir => '',);
+
+for my $test (@tests) {
+    $test->{exit_status} //= 0;
+    $test->{stdout}      //= qr/^$/;
+    $test->{stderr}      //= qr/^$/;
+
+    # NOTE Test::Cmd uses the shell which in turn might add environment
+    # variables depending on what rc contains
+    local %ENV;
+    @ENV{ keys %{ $test->{env} } } = values %{ $test->{env} };
+
+    $testcmd->run(exists $test->{args} ? (args => $test->{args}) : ());
+
+    my $args = $test->{args} // '';
+
+    exit_is($?, $test->{exit_status}, "STATUS $test_prog $args");
+    ok($testcmd->stdout =~ m/$test->{stdout}/, "STDOUT $test_prog $args")
+      or diag 'STDOUT: ' . ($testcmd->{stdout} // '');
+    ok($testcmd->stderr =~ m/$test->{stderr}/, "STDERR $test_prog $args")
+      or diag 'STDERR: ' . ($testcmd->{stderr} // '');
+}
+done_testing(@tests * 3);
--- network/validate-rsnapshot.c
+++ network/validate-rsnapshot.c
@@ -1,31 +1,42 @@
 /* validate-rsnapshot - check SSH_ORIGINAL_COMMAND for rsnapshot */
 
+#include <err.h>
 #include <errno.h>
+#include <getopt.h>
 #include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #ifdef SYSLOG
 #include <syslog.h>
 #endif
+#include <sysexits.h>
 #include <unistd.h>
 
 #include <pcre.h>
 
+int Flag_Preview;               /* -n */
+
 /* NOTE this restricts the length of the options (to prevent the remote
  * from saying -vv...) and the length of the filesystem path and greatly
  * restricts the characters that that path may contain */
+/* NOTE the flags may change randomly depending on the rsync version as
+ * they did between Centos 7.4 and Centos 7.5 */
 #define SUBSTRINGS 7
 char *pattern =
     "^(rsync) (--server) (--sender) (-[vnlHogDtprRxSe.iLsfxC]{1,24})"
-    " (--numeric-ids) ([.]) (/[A-Za-z0-9+_./-]{1,1024})$";
+    " (--numeric-ids) ([.]) ([A-Za-z0-9+_./][A-Za-z0-9+_./-]{0,1023})$";
 #define MAX_CMD_LEN 1024+24+42+1
 
 #define MAX_OFFSETS ((1+SUBSTRINGS)*3)
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    char *client = "unknown";
+#ifdef SYSLOG
+    char *client;
+#endif
     char *command[SUBSTRINGS + 1], *soc;
+    int ch;
 
     const char *error;
     int erroffset, i, rc;
@@ -34,19 +45,21 @@ int main(void)
 
 #ifdef __OpenBSD__
     if (pledge("exec rpath stdio", NULL) == -1)
-        return 1;
+        err(1, "pledge failed");
 #endif
 
 #ifdef SYSLOG
     openlog("validate-rsnapshot", LOG_NDELAY, LOG_AUTHPRIV);
     client = getenv("SSH_CLIENT");
+    if (client == NULL)
+        client = "unknown";
 #endif
 
     if ((soc = getenv("SSH_ORIGINAL_COMMAND")) == NULL) {
 #ifdef SYSLOG
         syslog(LOG_NOTICE, "no SSH_ORIGINAL_COMMAND set for %s", client);
 #endif
-        return 1;
+        exit(2);
     }
 
     /* precompiling the pattern gives a 2% speed increase, slightly
@@ -56,7 +69,7 @@ int main(void)
         syslog(LOG_ERR, "pcre compile failed at offset %d: %s ??", erroffset,
                error);
 #endif
-        return 1;
+        exit(3);
     }
 
     rc = pcre_exec(re, NULL, soc, strnlen(soc, MAX_CMD_LEN), 0, 0, offsets,
@@ -65,15 +78,17 @@ int main(void)
 #ifdef SYSLOG
         syslog(LOG_NOTICE, "match failed for %s", client);
 #endif
-        return 1;
+        exit(4);
     }
+
+// TODO that there are not ../ runs in path
 
     if (access(soc + offsets[2 * SUBSTRINGS], R_OK | X_OK) != 0) {
 #ifdef SYSLOG
         syslog(LOG_NOTICE, "not allowed to access '%s'",
                soc + offsets[2 * SUBSTRINGS]);
 #endif
-        return 1;
+        exit(5);
     }
 
     for (i = 1; i <= SUBSTRINGS; i++) {
@@ -82,10 +97,24 @@ int main(void)
     }
     command[SUBSTRINGS] = (char *) 0;
 
+    while ((ch = getopt(argc, argv, "n")) != -1) {
+        switch (ch) {
+        case 'n':
+            Flag_Preview = 1;
+            break;
+        default:
+            errx(EX_USAGE, "only -n flag is supported");
+        }
+    }
+
+    if (Flag_Preview) {
+        puts(soc + offsets[2 * SUBSTRINGS]);
+        exit(EXIT_SUCCESS);
+    }
     execvp(command[0], command);
 
 #ifdef SYSLOG
     syslog(LOG_NOTICE, "could not exec '%s' (%d)", command[0], errno);
 #endif
-    return 1;
+    exit(6);
 }
