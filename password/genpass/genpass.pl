02431eb787d630a8927eb8fa990f74b07f2cacd8
Jeremy Mates <jeremy.mates@gmail.com>
2019-11-04 21:26:29 +0000

    use sysopen to prevent I/O layer foo

--- password/genpass/genpass.pl
+++ password/genpass/genpass.pl
@@ -1,17 +1,19 @@
 #!/usr/bin/env perl
 use 5.16.0;
 use warnings;
+use Fcntl qw(O_RDONLY);
 
 my @code2char =
   qw(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 + /);
 
 sub genpass {
     my $length = shift;
-    open my $fh, '<', '/dev/urandom' or die "open /dev/urandom failed: $!\n";
+    sysopen my $fh, '/dev/urandom', O_RDONLY
+      or die "open /dev/urandom failed: $!\n";
     my $buf;
     sysread $fh, $buf, $length;
     my $pass = '';
-    for my $c ( unpack "C*", $buf ) {
+    for my $c (unpack "C*", $buf) {
         $pass .= $code2char[ $c % @code2char ];
     }
     die "insufficient entropy for password??" if length $pass != $length;
